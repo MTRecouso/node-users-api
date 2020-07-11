4263836d1a0491bbec1dfaa329d0ede89392e1f5
MTRecouso <mateus.recouso@hotmail.com>
2020-07-11 15:06:24 -0300

    Updated tests for user mapper to remove password from response

--- src/mappers/userMapper.test.js
+++ src/mappers/userMapper.test.js
@@ -7,21 +7,27 @@ const modelUser = {
   phones: [{ number: '123456789', areaCode: '11' }],
 };
 
-const httpUser = {
+const httpRequestUser = {
   nome: 'Teste',
   email: 'teste@teste.com',
   senha: 'teste234',
   telefones: [{ numero: '123456789', ddd: '11' }],
 };
 
+const httpResponseUser = {
+  nome: 'Teste',
+  email: 'teste@teste.com',
+  telefones: [{ numero: '123456789', ddd: '11' }],
+};
+
 describe('User mapper tests', () => {
   it('Map http user to model user', () => {
-    const mappedUser = UserMapper.httpToModel(httpUser);
+    const mappedUser = UserMapper.httpToModel(httpRequestUser);
     expect(mappedUser).toEqual(modelUser);
   });
 
   it('Map model user to http user', () => {
     const mappedUser = UserMapper.modelToHttp(modelUser);
-    expect(mappedUser).toEqual(httpUser);
+    expect(mappedUser).toMatchObject(httpResponseUser);
   });
 });
