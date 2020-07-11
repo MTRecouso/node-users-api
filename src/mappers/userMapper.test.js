f84c87122595d9d0cf6f387cd5c4010d9bbcfbd6
MTRecouso <mateus.recouso@hotmail.com>
2020-07-11 16:10:33 -0300

    Separated input from expected objects on user mapper tests

--- src/mappers/userMapper.test.js
+++ src/mappers/userMapper.test.js
@@ -1,6 +1,17 @@
 const UserMapper = require('./userMapper');
 
-const modelUser = {
+const inputUserModel = {
+  name: 'Teste',
+  email: 'teste@teste.com',
+  phones: [{ number: '123456789', areaCode: '11' }],
+  updatedAt: new Date(),
+  createdAt: new Date(),
+  lastLogin: new Date(),
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+  guid: '979b4a5f-32ea-4c9e-9be3-1984d316ab94',
+};
+
+const expectedUserModel = {
   name: 'Teste',
   email: 'teste@teste.com',
   password: 'teste234',
@@ -28,11 +39,11 @@ const httpResponseUser = {
 describe('User mapper tests', () => {
   it('Map http user to model user', () => {
     const mappedUser = UserMapper.httpToModel(httpRequestUser);
-    expect(mappedUser).toEqual(modelUser);
+    expect(mappedUser).toEqual(expectedUserModel);
   });
 
   it('Map model user to http user', () => {
-    const mappedUser = UserMapper.modelToHttp(modelUser);
+    const mappedUser = UserMapper.modelToHttp(inputUserModel);
     expect(mappedUser).toEqual(httpResponseUser);
   });
 });
