a883b332ffeb9ca352dd9e14a5985d329eaf6a14
MTRecouso <mateus.recouso@hotmail.com>
2020-07-11 14:25:16 -0300

    Updated user model tests with token, guid and hashed password

--- src/models/user.test.js
+++ src/models/user.test.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const UserModel = require('./user.js');
 
 const userData = {
+  guid: '2b6a6207-86d1-4213-8510-2121fb1239d4',
   name: 'Teste',
   email: 'teste@teste.com',
-  password: 'teste234',
   phones: [{ number: '123456789', areaCode: '11' }],
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
 };
 
 describe('User Model Tests', () => {
@@ -25,11 +26,13 @@ describe('User Model Tests', () => {
     const user = new UserModel(userData);
     const savedUser = await user.save();
     expect(savedUser._id).toBeDefined();
+    expect(savedUser.guid).toBeDefined();
     expect(savedUser.name).toBe(userData.name);
     expect(savedUser.email).toBe(userData.email);
-    expect(savedUser.password).toBe(userData.password);
+    expect(savedUser.password).toBeDefined();
     expect(savedUser.phones[0].number).toEqual(userData.phones[0].number);
     expect(savedUser.phones[0].areaCode).toEqual(userData.phones[0].areaCode);
+    expect(savedUser.token).toEqual(userData.token);
   });
 
   it('should fail on user creation because email already exists', async () => {
