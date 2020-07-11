a5a59eff1e5941c84d63bcca4ca1d316fbc70c75
MTRecouso <mateus.recouso@hotmail.com>
2020-07-11 15:50:48 -0300

    Updated user to use guid and hashed pasword

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+const uuid = require('uuid');
+
+const { hashPassword } = require('../services/passwordService');
 
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
+  guid: {
+    type: String,
+    required: true,
+    immutable: true,
+    default: uuid.v4(),
+  },
   name: {
     type: String,
     required: true,
@@ -15,11 +24,30 @@ const UserSchema = new Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
   phones: {
     type: [{ number: String, areaCode: String }],
     required: true,
   },
+  lastLogin: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
+
+UserSchema.pre('save', async function updatePasswordIfNeeded(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await hashPassword(user.password);
+  } else {
+    next();
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- src/models/user.test.js
+++ src/models/user.test.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const UserModel = require('./user.js');
 
-const userData = {
-  guid: '2b6a6207-86d1-4213-8510-2121fb1239d4',
+const createdUser = {
   name: 'Teste',
   email: 'teste@teste.com',
   phones: [{ number: '123456789', areaCode: '11' }],
   token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
 };
+
+const userData = { password: 'teste234', ...createdUser };
 
 describe('User Model Tests', () => {
   beforeAll(async () => {
@@ -25,14 +26,10 @@ describe('User Model Tests', () => {
   it('should create & save user successfully', async () => {
     const user = new UserModel(userData);
     const savedUser = await user.save();
+    expect(savedUser.toJSON()).toMatchObject(createdUser);
     expect(savedUser._id).toBeDefined();
     expect(savedUser.guid).toBeDefined();
-    expect(savedUser.name).toBe(userData.name);
-    expect(savedUser.email).toBe(userData.email);
     expect(savedUser.password).toBeDefined();
-    expect(savedUser.phones[0].number).toEqual(userData.phones[0].number);
-    expect(savedUser.phones[0].areaCode).toEqual(userData.phones[0].areaCode);
-    expect(savedUser.token).toEqual(userData.token);
   });
 
   it('should fail on user creation because email already exists', async () => {
