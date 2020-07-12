6fd5a148c2960f18b2d3573eb6097ab5219e3bf4
MTRecouso <mateus.recouso@hotmail.com>
2020-07-12 10:54:35 -0300

    Added gitignore, env example and removed comments from validator

--- src/validators/signInValidator.js
+++ src/validators/signInValidator.js
@@ -2,15 +2,11 @@ const { checkSchema } = require('express-validator');
 
 module.exports = checkSchema({
   email: {
-    // The location of the field, can be one or more of body, cookies, headers, params or query.
-    // If omitted, all request locations will be checked
     in: ['body'],
     errorMessage: 'Email inválido',
     isEmail: true,
   },
   senha: {
-    // The location of the field, can be one or more of body, cookies, headers, params or query.
-    // If omitted, all request locations will be checked
     in: ['body'],
     errorMessage: 'Senha inválida',
     isString: true,
